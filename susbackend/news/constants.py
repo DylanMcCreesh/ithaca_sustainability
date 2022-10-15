8f3040bb94c801988760d7d299c0bf3c5340b93b
Marya Kim <markim1815@gmail.com>
2022-10-15 14:42:15 -0400

    set up heroku

--- susbackend/news/constants.py
+++ susbackend/news/constants.py
@@ -1,1 +1,3 @@
-NEWS_API_URL = "https://newsapi.org/v2/everything?q=crypto&apiKey=06871c6b394f4c9198bfc4629a14b9ff"
+GLOBAL_NEWS_URL = "https://newsapi.org/v2/everything?q=sustainability&apiKey=06871c6b394f4c9198bfc4629a14b9ff"
+
+ITHACA_NEWS_URL = "https://newsapi.org/v2/everything?q=sustainability%20ithaca&apiKey=06871c6b394f4c9198bfc4629a14b9ff"
