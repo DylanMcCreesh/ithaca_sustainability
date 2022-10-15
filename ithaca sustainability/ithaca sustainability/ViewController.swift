005fbb5caf7652927ae2b0215350ded65975503d
Annesh Ghosh Dastidar <adastidar81@gmail.com>
2022-10-14 22:13:53 -0400

    Created xcode proj

---
+++ ithaca sustainability/ithaca sustainability/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ithaca sustainability
+//
+//  Created by Annesh Ghosh Dastidar on 10/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
