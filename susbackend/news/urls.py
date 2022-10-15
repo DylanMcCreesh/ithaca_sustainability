3fa44c0f20cec7560eef352a335b7bd57dc40be4
Marya Kim <markim1815@gmail.com>
2022-10-15 16:09:43 -0400

    Add test route to news

--- susbackend/news/urls.py
+++ susbackend/news/urls.py
@@ -4,5 +4,6 @@ from news import views
 
 urlpatterns = [
     path("ithaca/", views.get_ithaca_api),
-    path("global/", views.get_global_api)
+    path("global/", views.get_global_api),
+    path("", views.test)
 ]
