4d04642b21c5eba80315cc5a00fcb715de7c03ee
Marya Kim <markim1815@gmail.com>
2022-10-15 01:26:10 -0400

    Make discussion models and corresp. migrations

---
+++ susbackend/discussion/models/comment_model.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .post_model import Post
+from django.utils import timezone
+
+"""
+Define Comment model.
+Many-to-one relationship with itself (HOT! SEXY!)
+"""
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING) #do nothing vs cascade?
+    text = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    comment_id = models.IntegerField(primary_key = True)
+    parent_id = models.ForeignKey('self', on_delete=models.DO_NOTHING)
+    post_id = models.ForeignKey(Post, on_delete = models.CASCADE)
---
+++ susbackend/discussion/models/user_model.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+"""
+BECAUSE THIS IS A DJANGO APP
+TECHNICALLY.... WE DON'T NEED THIS, RIGHT??? 
+"""
