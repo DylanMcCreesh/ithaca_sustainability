879f7c6ffc500488aaf2be650deedf47ec08e3b8
Annesh Ghosh Dastidar <adastidar81@gmail.com>
2022-10-15 16:06:38 -0400

    Completed skeletons

--- ithaca sustainability/ithaca sustainability/EditPostViewController.swift
+++ ithaca sustainability/ithaca sustainability/EditPostViewController.swift
@@ -5,4 +5,219 @@
 //  Created by Annesh Ghosh Dastidar on 10/15/22.
 //
 
+import UIKit
 import Foundation
+
+class EditPostViewController: UIViewController {
+    
+    var resourcesButton = UIButton()
+    var discussionButton = UIButton()
+    var newsButton = UIButton()
+    var navBarBackground = UILabel()
+    
+    var titleLabel = UILabel()
+    var descriptionLabel = UILabel()
+    
+    var postTitleLabel = UILabel()
+    var postTitleText = UITextField()
+    
+    var postDescriptionLabel = UILabel()
+    var postDescriptionText = UITextView()
+    
+    var publishButton = UIButton()
+    
+    var headerBackground = UILabel()
+    
+                
+    var loadedNewsScreen = NewsViewController()
+    var loadedDiscussionScreen = ViewController()
+    var loadedResourcesScreen = ResourcesViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 219/255, green: 227/255, blue: 217/255, alpha: 1)
+        
+        
+        self.navigationController?.setNeedsUpdateOfHomeIndicatorAutoHidden()
+        
+        headerBackground.backgroundColor = .white
+        headerBackground.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(headerBackground)
+        
+        titleLabel.text = "Edit"
+        titleLabel.font = .systemFont(ofSize: 28, weight: .bold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        descriptionLabel.text = "Ithaca Sustainability Q&A"
+        descriptionLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        descriptionLabel.textColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 1)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        postTitleLabel.text = "Title"
+        postTitleLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        postTitleLabel.textColor = .white
+        postTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(postTitleLabel)
+        
+        postTitleText.placeholder = "Enter Post Title"
+        postTitleText.backgroundColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 0.25)
+        postTitleText.translatesAutoresizingMaskIntoConstraints = false
+        postTitleText.setLeftPaddingPoints(10)
+        postTitleText.setRightPaddingPoints(10)
+        view.addSubview(postTitleText)
+        
+        postDescriptionLabel.text = "Description"
+        postDescriptionLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        postDescriptionLabel.textColor = .white
+        postDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(postDescriptionLabel)
+        
+        postDescriptionText.translatesAutoresizingMaskIntoConstraints = false
+        postDescriptionText.font = .systemFont(ofSize: 18, weight: .regular)
+        postDescriptionText.backgroundColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 0.25)
+        view.addSubview(postDescriptionText)
+        
+        publishButton.setBackgroundImage(UIImage(named:"save_button"), for: .normal)
+        publishButton.backgroundColor = .clear
+        publishButton.translatesAutoresizingMaskIntoConstraints = false
+        publishButton.addTarget(self, action: #selector(saveButtonPress), for: .touchUpInside)
+        view.addSubview(publishButton)
+        
+        navBarBackground.backgroundColor = .white
+        navBarBackground.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(navBarBackground)
+        
+        resourcesButton.setBackgroundImage(UIImage(named: "resources1"), for: .normal)
+        resourcesButton.backgroundColor = .white
+        resourcesButton.addTarget(self, action: #selector(resourcesButtonPress), for: .touchUpInside)
+        resourcesButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resourcesButton)
+        
+        discussionButton.setBackgroundImage(UIImage(named: "home2"), for: .normal)
+        discussionButton.backgroundColor = .white
+        discussionButton.addTarget(self, action: #selector(discussionButtonPress), for: .touchUpInside)
+        discussionButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(discussionButton)
+        
+        newsButton.setBackgroundImage(UIImage(named:"news1"), for: .normal)
+        newsButton.backgroundColor = .white
+        newsButton.translatesAutoresizingMaskIntoConstraints = false
+        newsButton.addTarget(self, action: #selector(newsButtonPress), for: .touchUpInside)
+        view.addSubview(newsButton)
+        
+        setupConstraints()
+    }
+
+    func setupConstraints() {
+//         Setup the constraints for our views
+        NSLayoutConstraint.activate([
+            headerBackground.bottomAnchor.constraint(equalTo: view.topAnchor, constant:(view.frame.height * 0.16)),
+            headerBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerBackground.topAnchor.constraint(equalTo: view.topAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -(view.frame.height * 0.02)),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.frame.height * 0.002),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postTitleLabel.topAnchor.constraint(equalTo: headerBackground.bottomAnchor, constant: view.frame.height * 0.025),
+            postTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+        ])
+        
+        NSLayoutConstraint.activate([
+            postTitleText.topAnchor.constraint(equalTo: postTitleLabel.bottomAnchor, constant: view.frame.height * 0.01),
+            postTitleText.leadingAnchor.constraint(equalTo: postTitleLabel.leadingAnchor),
+            postTitleText.heightAnchor.constraint(equalToConstant: 40),
+            postTitleText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+
+        ])
+        
+        NSLayoutConstraint.activate([
+            postDescriptionLabel.topAnchor.constraint(equalTo: postTitleText.bottomAnchor, constant: view.frame.height * 0.025),
+            postDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+        ])
+        
+        NSLayoutConstraint.activate([
+            postDescriptionText.topAnchor.constraint(equalTo: postDescriptionLabel.bottomAnchor, constant: view.frame.height * 0.01),
+            postDescriptionText.leadingAnchor.constraint(equalTo: postTitleLabel.leadingAnchor),
+            postDescriptionText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            postDescriptionText.heightAnchor.constraint(equalToConstant: 400),
+        ])
+        
+        NSLayoutConstraint.activate([
+            publishButton.topAnchor.constraint(equalTo: postDescriptionText.bottomAnchor, constant: view.frame.height * 0.025),
+            publishButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            publishButton.widthAnchor.constraint(equalToConstant: 300),
+            publishButton.heightAnchor.constraint(equalToConstant: 50),
+        
+        ])
+        
+        NSLayoutConstraint.activate([
+            navBarBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            navBarBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navBarBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navBarBackground.topAnchor.constraint(equalTo: discussionButton.topAnchor, constant: -(view.frame.height * 0.018)),
+        ])
+        
+        NSLayoutConstraint.activate([
+            resourcesButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            resourcesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.height * 0.05625),
+            resourcesButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            resourcesButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            discussionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            discussionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            discussionButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            discussionButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            newsButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            newsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.height * 0.05625)),
+            newsButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            newsButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        
+        
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @objc func newsButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedNewsScreen)
+    }
+    
+    @objc func resourcesButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedResourcesScreen)
+    }
+    
+    @objc func discussionButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedDiscussionScreen)
+    }
+    
+    @objc func saveButtonPress(){
+        //TODO
+    }
+    
+    @objc func refresh(){
+        //TODO
+    }
+}
+
+
+
--- ithaca sustainability/ithaca sustainability/StartupViewController.swift
+++ ithaca sustainability/ithaca sustainability/StartupViewController.swift
@@ -5,4 +5,173 @@
 //  Created by Annesh Ghosh Dastidar on 10/15/22.
 //
 
+
+import UIKit
 import Foundation
+import Alamofire
+
+class StartupViewController: UIViewController {
+    
+    var resourcesButton = UIButton()
+    var discussionButton = UIButton()
+    var newsButton = UIButton()
+    var navBarBackground = UILabel()
+    
+    var titleLabel = UILabel()
+    var descriptionLabel = UILabel()
+    
+    var treeImage = UIButton()
+    
+    var enterUsername = UITextField()
+    
+    var loginButton = UIButton()
+    var signupButton = UIButton()
+    
+    /*var postTitleLabel = UILabel()
+    var postTitleText = UITextField()
+    
+    var postDescriptionLabel = UILabel()
+    var postDescriptionText = UITextView()
+    
+    var publishButton = UIButton()
+    
+    var headerBackground = UILabel()*/
+    
+                
+    var loadedNewsScreen = NewsViewController()
+    var loadedDiscussionScreen = ViewController()
+    var loadedResourcesScreen = ResourcesViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 219/255, green: 227/255, blue: 217/255, alpha: 1)
+        
+        
+        self.navigationController?.setNeedsUpdateOfHomeIndicatorAutoHidden()
+        
+        
+        titleLabel.text = "App Name"
+        titleLabel.textColor = .white
+        titleLabel.font = .systemFont(ofSize: 28, weight: .bold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        descriptionLabel.text = "Ithaca Sustainability Q&A"
+        descriptionLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        descriptionLabel.textColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 1)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        treeImage.setBackgroundImage(UIImage(named: "white_tree"), for: .normal)
+        treeImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(treeImage)
+        
+        //enterUsername.placeholder = "Enter Username"
+        enterUsername.attributedPlaceholder = NSAttributedString(string: "Enter Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 236/255, green: 236/255, blue: 236/225, alpha: 1)])
+        enterUsername.textColor = .white
+        enterUsername.layer.borderColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 1).cgColor
+        enterUsername.layer.borderWidth = 1.5
+        enterUsername.backgroundColor = UIColor(red: 118/255, green: 158/255, blue: 125/225, alpha: 0.25)
+        enterUsername.translatesAutoresizingMaskIntoConstraints = false
+        enterUsername.setLeftPaddingPoints(10)
+        enterUsername.setRightPaddingPoints(10)
+        view.addSubview(enterUsername)
+        
+        loginButton.setBackgroundImage(UIImage(named: "login"), for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.addTarget(self, action: #selector(loginButtonPress), for: .touchUpInside)
+        view.addSubview(loginButton)
+        
+        signupButton.setBackgroundImage(UIImage(named: "signup"), for: .normal)
+        signupButton.translatesAutoresizingMaskIntoConstraints = false
+        signupButton.addTarget(self, action: #selector(signupButtonPress), for: .touchUpInside)
+        view.addSubview(signupButton)
+        
+        
+        
+        setupConstraints()
+    }
+
+    func setupConstraints() {
+//         Setup the constraints for our views
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -(view.frame.height * 0.02)),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.frame.height * 0.002),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            treeImage.widthAnchor.constraint(equalToConstant: 250),
+            treeImage.heightAnchor.constraint(equalToConstant: 250),
+            treeImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            treeImage.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 50),
+        
+        ])
+        
+        NSLayoutConstraint.activate([
+            enterUsername.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(view.frame.height * 0.25)),
+            enterUsername.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            enterUsername.widthAnchor.constraint(equalToConstant: 300),
+            enterUsername.heightAnchor.constraint(equalToConstant: 50),
+
+        ])
+        
+        NSLayoutConstraint.activate([
+            signupButton.topAnchor.constraint(equalTo: enterUsername.bottomAnchor, constant: 50),
+            signupButton.leadingAnchor.constraint(equalTo: enterUsername.leadingAnchor),
+            signupButton.widthAnchor.constraint(equalToConstant: 140),
+            signupButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: enterUsername.bottomAnchor, constant: 50),
+            loginButton.trailingAnchor.constraint(equalTo: enterUsername.trailingAnchor),
+            loginButton.widthAnchor.constraint(equalToConstant: 140),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @objc func newsButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedNewsScreen)
+    }
+    
+    @objc func resourcesButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedResourcesScreen)
+    }
+    
+    @objc func discussionButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedDiscussionScreen)
+    }
+    
+    @objc func publishButtonPress(){
+        //TODO
+    }
+    
+    @objc func loginButtonPress(){
+        
+    }
+    
+    @objc func signupButtonPress(){
+        
+    }
+    
+    @objc func refresh(){
+        //TODO
+    }
+}
+
