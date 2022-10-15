4c201104e621338afc21584d38386b2dcf10d9a3
DylanMcCreesh <dylanmccreesh@gmail.com>
2022-10-14 22:53:50 -0400

    Beginning Frontend Skeleton

---
+++ ithaca sustainability/ithaca sustainability/ResourcesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  ithaca sustainability
+//
+//  Created by Dylan McCreesh on 10/14/22.
+//
+
+import Foundation
+import UIKit
+
+class ResourcesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
