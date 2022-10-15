413fc10ca36415a392df9c041e17d8bc06f0b7e3
DylanMcCreesh <dylanmccreesh@gmail.com>
2022-10-15 10:57:54 -0400

    Pods

---
+++ ithaca sustainability/Pods/Target Support Files/Pods-ithaca sustainability-ithaca sustainabilityUITests/Pods-ithaca sustainability-ithaca sustainabilityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ithaca_sustainability_ithaca_sustainabilityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ithaca_sustainability_ithaca_sustainabilityUITests
+@end
---
+++ ithaca sustainability/Pods/Target Support Files/Pods-ithaca sustainability/Pods-ithaca sustainability-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ithaca_sustainability : NSObject
+@end
+@implementation PodsDummy_Pods_ithaca_sustainability
+@end
