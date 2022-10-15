cfdc6b9e6e976a3c47e2ea1882c45782d2259060
DylanMcCreesh <dylanmccreesh@gmail.com>
2022-10-15 00:15:03 -0400

    Screen Nav
    Basic navigation between 3 main screens.

--- ithaca sustainability/ithaca sustainability/NewsViewController.swift
+++ ithaca sustainability/ithaca sustainability/NewsViewController.swift
@@ -10,11 +10,75 @@ import UIKit
 
 class NewsViewController: UIViewController {
 
+    var resourcesButton = UIButton()
+    var discussionButton = UIButton()
+    var newsButton = UIButton()
+    
+    var loadedDiscussionScreen: ViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        view.backgroundColor = .white
+        
+        title = "News"
 
+        resourcesButton.setBackgroundImage(UIImage(named: "news1"), for: .normal)
+        resourcesButton.backgroundColor = .white
+        resourcesButton.addTarget(self, action: #selector(resourcesButtonPress), for: .touchUpInside)
+        resourcesButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resourcesButton)
+        
+        discussionButton.setBackgroundImage(UIImage(named: "news1"), for: .normal)
+        discussionButton.backgroundColor = .white
+        discussionButton.addTarget(self, action: #selector(discussionButtonPress), for: .touchUpInside)
+        discussionButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(discussionButton)
+        
+        newsButton.setBackgroundImage(UIImage(named:"news1"), for: .normal)
+        newsButton.backgroundColor = .white
+        newsButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newsButton)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+//         Setup the constraints for our views
+        
+        NSLayoutConstraint.activate([
+            resourcesButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            resourcesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.height * 0.05625),
+            resourcesButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            resourcesButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            discussionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            discussionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            discussionButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            discussionButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            newsButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            newsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.height * 0.05625)),
+            newsButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            newsButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @objc func discussionButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedDiscussionScreen!)
+    }
+    
+    @objc func resourcesButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedDiscussionScreen!.loadedResourcesScreen)
+    }
 
 }
 
--- ithaca sustainability/ithaca sustainability/ViewController.swift
+++ ithaca sustainability/ithaca sustainability/ViewController.swift
@@ -1,19 +1,88 @@
 //
 //  ViewController.swift
-//  ithaca sustainability
+//  practiceTableView
 //
-//  Created by Annesh Ghosh Dastidar on 10/14/22.
+//  Created by Dylan McCreesh on 10/14/22.
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var resourcesButton = UIButton()
+    var discussionButton = UIButton()
+    var newsButton = UIButton()
+                
+    var loadedNewsScreen = NewsViewController()
+    var loadedResourcesScreen = ResourcesViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        title = "Discussion"
+        
+        loadedNewsScreen.loadedDiscussionScreen = self
+        loadedResourcesScreen.loadedDiscussionScreen = self
+        
+        self.navigationController?.setNeedsUpdateOfHomeIndicatorAutoHidden()
+        
+        resourcesButton.setBackgroundImage(UIImage(named: "news1"), for: .normal)
+        resourcesButton.backgroundColor = .white
+        resourcesButton.addTarget(self, action: #selector(resourcesButtonPress), for: .touchUpInside)
+        resourcesButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resourcesButton)
+        
+        discussionButton.setBackgroundImage(UIImage(named: "news1"), for: .normal)
+        discussionButton.backgroundColor = .white
+        discussionButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(discussionButton)
+        
+        newsButton.setBackgroundImage(UIImage(named:"news1"), for: .normal)
+        newsButton.backgroundColor = .white
+        newsButton.translatesAutoresizingMaskIntoConstraints = false
+        newsButton.addTarget(self, action: #selector(newsButtonPress), for: .touchUpInside)
+        view.addSubview(newsButton)
+        
+        setupConstraints()
     }
 
-
+    func setupConstraints() {
+//         Setup the constraints for our views
+        
+        NSLayoutConstraint.activate([
+            resourcesButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            resourcesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.height * 0.05625),
+            resourcesButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            resourcesButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            discussionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            discussionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            discussionButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            discussionButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+        
+        NSLayoutConstraint.activate([
+            newsButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            newsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.height * 0.05625)),
+            newsButton.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+            newsButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05625),
+        ])
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @objc func newsButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedNewsScreen)
+    }
+    
+    @objc func resourcesButtonPress(){
+        self.view.window?.rootViewController = UINavigationController(rootViewController: self.loadedResourcesScreen)
+    }
 }
 
