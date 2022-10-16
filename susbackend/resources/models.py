b741dd7d841b91ebebf6d2ead405a6560c6c84a1
Marya Kim <markim1815@gmail.com>
2022-10-16 05:15:24 -0400

    Test resources serializers

--- susbackend/resources/models.py
+++ susbackend/resources/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     """
     title = models.CharField(max_length=250)
 
+    def __str__(self): 
+        return '%s' % (self.title)
+
 
 class Brand(models.Model):
     """
