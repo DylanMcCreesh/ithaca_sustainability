c708b519710c0f84ba96568025813a34b8f71f10
Marya Kim <markim1815@gmail.com>
2022-10-16 01:46:21 -0400

    Implement news api

--- susbackend/news/urls.py
+++ susbackend/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from news import views
 
 urlpatterns = [
-    path("", views.get_api)
+    path("ithaca/", views.get_ithaca),
+    path("global/", views.get_global)
 ]
--- susbackend/news/views.py
+++ susbackend/news/views.py
@@ -1,20 +1,45 @@
+from pickle import GLOBAL
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+import time
 
-from django.http import JsonResponse
+from news.constants import ITHACA_URL, GLOBAL_URL
 
-from news.constants import NEWS_API_URL
-
+class db: 
+    ithaca_articles = None
+    global_articles = None
+    time_since_ithaca = 0
+    time_since_global = 0
 
 @api_view(['GET'])
-def get_api(request):
+def get_ithaca(request):
+    if db.time_since_ithaca > time.time() - 1799:
+        return db.ithaca_articles
+
     try:
-        response = requests.get(NEWS_API_URL)
+        response = requests.get(ITHACA_URL)
     except Exception as e:
         raise e 
 
-    return Response(response.json(), status=status.HTTP_200_OK)
-    #return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+    db.ithaca_articles = response.json()
+    db.time_since_ithaca = time.time()
+
+    return Response(db.ithaca_articles, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_global(request):
+    if db.time_since_global > time.time() - 1799:
+        return db.global_articles
+
+    try:
+        response = requests.get(GLOBAL_URL)
+    except Exception as e:
+        raise e 
+
+    db.global_articles = response.json()
+    db.time_since_global = time.time()
+
+    return Response(db.global_articles, status=status.HTTP_200_OK)
